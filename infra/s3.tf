2980e066578a7c5db76b6b8e42f540f8790c1ce5
Elias Shaik <shaikelias9@gmail.com>
2026-05-01 15:50:08 -0500

    s3bucketchangeforglue

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,6 +1,6 @@
 # Create the S3 Bucket
-resource "aws_s3_bucket" "cicdbucket" {
-  bucket = "elias-shaik-cicd-bucket"
+resource "aws_s3_bucket" "cicd-bucket" {
+  bucket = "s3-terraform-cicd-bucket-dev"
 
   tags = {
     Name        = "s3-terraform-cicd-bucket-dev"
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "cicdbucket" {
 
 # Create the S3 Bucket
 resource "aws_s3_bucket" "gluescripts" {
-  bucket = "glue-scripts-application-dev-bucket"
+  bucket = "s3-glue-scripts-application-dev-bucket"
 
   tags = {
     Name        = "s3-glue-scripts-application-dev-bucket"
